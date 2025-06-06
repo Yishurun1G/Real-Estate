df96fb2d3361523480ac9f20a74f66c5bd8a118f
yishurun getachew <youremail@example.com"yishurungetachew@gmail.com>
2025-06-06 18:26:32 +0300

    Another commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,6 +11,10 @@ if(showMobileMenu){
 
    document.body.style.overflow = 'auto'
 }
+return ()=>{
+document.body.style.overflow = 'auto'
+
+}
 },[showMobileMenu])
 
 
@@ -21,10 +25,10 @@ if(showMobileMenu){
        items-center py-4 px-6 md:px-20 lg:px-32 bg-transparent">
         <img src={assets.logo} alt="" />
         <ul className='hidden md:flex gap-7 text-white'>
-          <a href="Header" className="cursor-pointer hover:text-gray-400">Home</a>
-          <a href="Header" className="cursor-pointer hover:text-gray-400">About</a>
-          <a href="Header" className="cursor-pointer hover:text-gray-400">Projects</a>
-          <a href="Header" className="cursor-pointer hover:text-gray-400">Testimonials</a>
+          <a href="#Header" className="cursor-pointer hover:text-gray-400">Home</a>
+          <a href="#About" className="cursor-pointer hover:text-gray-400">About</a>
+          <a href="#Projects" className="cursor-pointer hover:text-gray-400">Projects</a>
+          <a href="#Testimonials" className="cursor-pointer hover:text-gray-400">Testimonials</a>
         </ul>
         <button className="hidden md:block bg-white px-8
         py-2 rounded-full">Sign Up</button>
@@ -43,13 +47,13 @@ if(showMobileMenu){
 </div>
 
         <ul className='flex flex-col items-center gap-2 mt-5 px-5 text-lg'>
-<a href="#Header" className='px-4 py-2 rounded-full
+<a onClick={()=> setMobileMenu(false)} href="#Header" className='px-4 py-2 rounded-full
 inline-block '>Home</a>
-<a href="#About" className='px-4 py-2 rounded-full
+<a onClick={()=> setMobileMenu(false)} href="#About" className='px-4 py-2 rounded-full
 inline-block '>About</a>
-<a href="#Projects" className='px-4 py-2 rounded-full
+<a onClick={()=> setMobileMenu(false)} href="#Projects" className='px-4 py-2 rounded-full
 inline-block '>Projects</a>
-<a href="#Testimonials" className='px-4 py-2 rounded-full
+<a onClick={()=> setMobileMenu(false)} href="#Testimonials" className='px-4 py-2 rounded-full
 inline-block '>Testimonials</a>
         </ul>
       </div>
